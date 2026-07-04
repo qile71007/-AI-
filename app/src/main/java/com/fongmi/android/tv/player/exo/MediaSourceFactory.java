3ce269d9788099dfae89e2ed77027a90105542e0
七乐 <15843701456@163.com>
2026-07-05 07:40:07 +0800

    unzipgit

---
+++ app/src/main/java/com/fongmi/android/tv/player/exo/MediaSourceFactory.java
@@ -0,0 +1,138 @@
+package com.fongmi.android.tv.player.exo;
+
+import static androidx.media3.extractor.ts.DefaultTsPayloadReaderFactory.FLAG_ENABLE_HDMV_DTS_AUDIO_STREAMS;
+
+import androidx.annotation.NonNull;
+import androidx.media3.common.C;
+import androidx.media3.common.MediaItem;
+import androidx.media3.database.StandaloneDatabaseProvider;
+import androidx.media3.datasource.DataSource;
+import androidx.media3.datasource.DefaultDataSource;
+import androidx.media3.datasource.HttpDataSource;
+import androidx.media3.datasource.cache.Cache;
+import androidx.media3.datasource.cache.CacheDataSource;
+import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor;
+import androidx.media3.datasource.cache.SimpleCache;
+import androidx.media3.datasource.okhttp.OkHttpDataSource;
+import androidx.media3.exoplayer.drm.DrmSessionManagerProvider;
+import androidx.media3.exoplayer.source.ConcatenatingMediaSource2;
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
+import androidx.media3.exoplayer.source.MediaSource;
+import androidx.media3.exoplayer.upstream.LoadErrorHandlingPolicy;
+import androidx.media3.extractor.DefaultExtractorsFactory;
+import androidx.media3.extractor.ExtractorsFactory;
+import androidx.media3.extractor.ts.TsExtractor;
+
+import com.fongmi.android.tv.App;
+import com.fongmi.android.tv.setting.PreloadSetting;
+import com.fongmi.android.tv.utils.FileUtil;
+import com.fongmi.android.tv.utils.UrlUtil;
+import com.github.catvod.net.OkHttp;
+import com.github.catvod.utils.Path;
+
+import java.io.File;
+import java.util.Map;
+
+public class MediaSourceFactory implements MediaSource.Factory {
+
+    private static final int CACHE_SPACE_PERCENT = 80;
+    private static final String CONCAT_SOURCE_SEPARATOR = "***";
+    private static final String CONCAT_SOURCE_SEPARATOR_REGEX = "\\*\\*\\*";
+    private static final String CONCAT_DURATION_SEPARATOR = "|||";
+    private static final String CONCAT_DURATION_SEPARATOR_REGEX = "\\|\\|\\|";
+
+    private static StandaloneDatabaseProvider databaseProvider;
+    private static Cache cache;
+
+    private final DefaultMediaSourceFactory defaultMediaSourceFactory;
+    private HttpDataSource.Factory httpDataSourceFactory;
+    private DataSource.Factory dataSourceFactory;
+    private ExtractorsFactory extractorsFactory;
+
+    public MediaSourceFactory() {
+        defaultMediaSourceFactory = new DefaultMediaSourceFactory(getDataSourceFactory(), getExtractorsFactory());
+    }
+
+    static DataSource.Factory createUpstreamDataSourceFactory(Map<String, String> headers) {
+        HttpDataSource.Factory factory = new OkHttpDataSource.Factory(OkHttp.player());
+        factory.setDefaultRequestProperties(headers);
+        return new DefaultDataSource.Factory(App.get(), factory);
+    }
+
+    static synchronized Cache getCache() {
+        if (cache != null) return cache;
+        File dir = Path.exoCache();
+        return cache = new SimpleCache(dir, new LeastRecentlyUsedCacheEvictor(getMaxCacheSize(dir)), getDatabaseProvider());
+    }
+
+    private static StandaloneDatabaseProvider getDatabaseProvider() {
+        if (databaseProvider == null) databaseProvider = new StandaloneDatabaseProvider(App.get());
+        return databaseProvider;
+    }
+
+    private static long getMaxCacheSize(File dir) {
+        long usedBytes = FileUtil.getDirectorySize(dir);
+        long availableBytes = Math.max(0, FileUtil.getAvailableStorageSpace(dir));
+        long storageBudget = (usedBytes + availableBytes) * CACHE_SPACE_PERCENT / 100;
+        return Math.min(PreloadSetting.getPreloadSizeBytes(), storageBudget);
+    }
+
+    static boolean isConcatenatingUrl(String url) {
+        return url != null && url.contains(CONCAT_SOURCE_SEPARATOR) && url.contains(CONCAT_DURATION_SEPARATOR);
+    }
+
+    @NonNull
+    @Override
+    public MediaSource.Factory setDrmSessionManagerProvider(@NonNull DrmSessionManagerProvider drmSessionManagerProvider) {
+        return this;
+    }
+
+    @NonNull
+    @Override
+    public MediaSource.Factory setLoadErrorHandlingPolicy(@NonNull LoadErrorHandlingPolicy loadErrorHandlingPolicy) {
+        return this;
+    }
+
+    @NonNull
+    @Override
+    public @C.ContentType int[] getSupportedTypes() {
+        return defaultMediaSourceFactory.getSupportedTypes();
+    }
+
+    @NonNull
+    @Override
+    public MediaSource createMediaSource(@NonNull MediaItem mediaItem) {
+        getHttpDataSourceFactory().setDefaultRequestProperties(ExoUtil.extractHeaders(mediaItem));
+        String url = mediaItem.requestMetadata.mediaUri != null ? mediaItem.requestMetadata.mediaUri.toString() : "";
+        if (isConcatenatingUrl(url)) return createConcatenatingMediaSource(mediaItem, url);
+        else return defaultMediaSourceFactory.createMediaSource(mediaItem);
+    }
+
+    private MediaSource createConcatenatingMediaSource(MediaItem mediaItem, String url) {
+        ConcatenatingMediaSource2.Builder builder = new ConcatenatingMediaSource2.Builder();
+        for (String split : url.split(CONCAT_SOURCE_SEPARATOR_REGEX)) {
+            String[] info = split.split(CONCAT_DURATION_SEPARATOR_REGEX);
+            if (info.length >= 2) builder.add(defaultMediaSourceFactory.createMediaSource(mediaItem.buildUpon().setUri(UrlUtil.uri(info[0])).build()), Long.parseLong(info[1]));
+        }
+        return builder.build();
+    }
+
+    private ExtractorsFactory getExtractorsFactory() {
+        if (extractorsFactory == null) extractorsFactory = new DefaultExtractorsFactory().setTsExtractorFlags(FLAG_ENABLE_HDMV_DTS_AUDIO_STREAMS).setTsExtractorTimestampSearchBytes(TsExtractor.DEFAULT_TIMESTAMP_SEARCH_BYTES * 10);
+        return extractorsFactory;
+    }
+
+    private DataSource.Factory getDataSourceFactory() {
+        if (dataSourceFactory == null) dataSourceFactory = getCacheDataSource(new DefaultDataSource.Factory(App.get(), getHttpDataSourceFactory()));
+        return dataSourceFactory;
+    }
+
+    private CacheDataSource.Factory getCacheDataSource(DataSource.Factory upstreamFactory) {
+        return new CacheDataSource.Factory().setCache(getCache()).setUpstreamDataSourceFactory(upstreamFactory).setCacheWriteDataSinkFactory(null).setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR);
+    }
+
+    private HttpDataSource.Factory getHttpDataSourceFactory() {
+        if (httpDataSourceFactory == null) httpDataSourceFactory = new OkHttpDataSource.Factory(OkHttp.player());
+        return httpDataSourceFactory;
+    }
+}
