9859f44c6f4942eff90f4b69038b4d8e64b79f9f
七乐 <15843701456@163.com>
2026-07-05 11:02:19 +0800

    Update Setting.java

--- app/src/main/java/com/fongmi/android/tv/setting/Setting.java
+++ app/src/main/java/com/fongmi/android/tv/setting/Setting.java
@@ -594,4 +594,29 @@ public class Setting {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) return false;
         return new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, Uri.parse("package:" + App.get().getPackageName())).resolveActivity(App.get().getPackageManager()) != null || new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION).resolveActivity(App.get().getPackageManager()) != null;
     }
+
+    // ========== WebDAV 配置 ==========
+    public static String getWebdavUrl() {
+        return Prefers.getString("webdav_url", "");
+    }
+
+    public static void putWebdavUrl(String url) {
+        Prefers.put("webdav_url", url);
+    }
+
+    public static String getWebdavUser() {
+        return Prefers.getString("webdav_user", "");
+    }
+
+    public static void putWebdavUser(String user) {
+        Prefers.put("webdav_user", user);
+    }
+
+    public static String getWebdavPass() {
+        return Prefers.getString("webdav_pass", "");
+    }
+
+    public static void putWebdavPass(String pass) {
+        Prefers.put("webdav_pass", pass);
+    }
 }
