7bf11192a3fe7c0510c987d1e736791d7586e99b
七乐 <15843701456@163.com>
2026-07-28 08:16:40 +0800

    Update Clock.java

--- app/src/main/java/com/fongmi/android/tv/utils/Clock.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Clock.java
@@ -22,7 +22,7 @@ public class Clock {
     }
 
     public static Clock create(TextView view) {
-        return new Clock().view(view).format("HH:mm:ss");
+        return new Clock().view(view).format("yyyy年MM月dd日 EEEE HH点mm分ss秒");
     }
 
     public Clock() {
